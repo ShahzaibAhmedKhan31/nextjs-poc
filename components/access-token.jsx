65b114b5b11a1b11e382c5b27a1a72b18116699c
Shahzaib Ahmed Khan <shahzaib.aleem@tpsonline.com>
2023-10-06 16:44:29 +0500

    error resolved

--- components/access-token.jsx
+++ components/access-token.jsx
@@ -2,7 +2,7 @@ import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Component() {
   const { data } = useSession()
-  //const { accessToken } = data
+  const { accessToken } = data
   console.log(data);
-  return <div>Access Token: {data.accessToken}</div>
+  return <div>Access Token: {accessToken}</div>
 }
